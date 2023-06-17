f277c68e6276c888f8620d4ec16028aaad210d60
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-17 20:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let table = {
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz",
+	}
+
+	let solutions =[];
+	function solve(currIndex, calculatedString){
+		if(input.length <= currIndex){
+	           solutions.push(calculatedString);
+			return;
+		}
+		let currChar = input[currIndex];
+		for(let i=0; i<table[currChar].length; i++){
+			solve(currIndex+1, calculatedString + table[currChar]);
+		}
+	}
+	solve(0, "");
+	return solutions;
 }
 
 module.exports = letterCombinations;
